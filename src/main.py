a2b8f21868257b0197e492a61bfd9aa1aabf22e5
Varisht Ghedia <varishtsg@gmail.com>
2023-07-29 16:44:25 +0530

    Removed GPS Bits

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import serial
 import datetime as dt
 from picamera import PiCamera
 from gps_utils import parseGPS, createFilenames, convertToMP4 
@@ -11,23 +10,13 @@ VIDEO_RATE = 25 # framerate
 
 def main():
     port = "/dev/serial0"
-    recording_path = "/media/usb/Dashcam/Recordings/"
-    logging_path = "/media/usb/Dashcam/Logs/"
+    recording_path = "/home/carpi/Dashcam/Recordings/"
+    logging_path = "/home/carpi/Dashcam/Logs/"
     gps_dict = {
         "Date" : "xx/xx/xxxx",
-        "Time" : "xx:xx:xx",
-        "Latitude"  : "xxxx",
-        "LatDirection" : "x",
-        "Longitude" : "xxxx",
-        "LongDirection" : "x",
-        "Speed" : "xx",
-        "TrueCourse" : "xxx",
-        "Altitude" : "xxx"
+        "Time" : "xx:xx:xx"
     }
     
-    # initialize serial port
-    ser = serial.Serial(port, baudrate = 9600, timeout = 0.5)
-
     # initialize the camera and grab a reference 
     camera = PiCamera() 
     camera.resolution = (1280, 720) 
@@ -42,14 +31,9 @@ def main():
             camera.start_recording(record_file) 
              
             while (dt.datetime.now() - start).seconds < VIDEO_LENGTH: 
-                # Getting GPS data. Converting from bytes to str
-                data = ser.readline()
-                data = data.decode('ascii', errors='replace').strip()
-                if data[0:6] == "$GNRMC" or data[0:6] == "$GNGGA":
-                    gps_dict = parseGPS(data, log_file, gps_dict)
 
                 # Overlay the data on each frame
-                text = "{} {}, Lt: {}({}), Lg: {}({}), {} MPH, TC: {}, Alt: {} m".format(gps_dict["Date"], gps_dict["Time"], gps_dict["Latitude"], gps_dict["LatDirection"], gps_dict["Longitude"], gps_dict["LongDirection"], gps_dict["Speed"], gps_dict["TrueCourse"], gps_dict["Altitude"])
+                text = f"{gps_dict['Date']} {gps_dict['Time']}"
                 camera.annotate_text = text
 
             print("Stopped " + record_file)
